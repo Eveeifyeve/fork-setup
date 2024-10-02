bc9fef0e48c16c9842cde12aed6c915e5efecb7f
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-03 02:26:43 +1000

    fix: shell

--- flake.nix
+++ flake.nix
@@ -1,27 +1,33 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
-  outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        {
-          config,
-          self',
-          inputs',
-          lib,
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          formatter = pkgs.nixfmt-rfc-style;
-          devShells = {
-            tauri-docs = ./projects/tauri-docs.nix;
-          };
-        };
-    };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-parts.url = "github:hercules-ci/flake-parts";
+	};
+	outputs =
+		inputs@{ flake-parts, nixpkgs, ... }:
+		flake-parts.lib.mkFlake { inherit inputs; } {
+			systems = nixpkgs.lib.systems.flakeExposed;
+			perSystem =
+			{
+				config,
+				self',
+				inputs',
+				lib,
+				pkgs,
+				system,
+				...
+			}:
+			{
+				formatter = pkgs.nixfmt-rfc-style;
+				devShells = {
+					tauri-docs = pkgs.mkShell {
+						packages = with pkgs; [
+							nodejs
+							pnpm
+						];
+					};
+					# Feel free to add more devshells here.
+				};
+			};
+		};
 }
